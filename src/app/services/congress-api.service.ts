4cdd505a3c81e826de8a2f70bfb09db85d1207eb
Rodry <rodry.sauco@gmail.com>
2020-07-04 22:26:28 -0300

    changed functions

--- src/app/services/congress-api.service.ts
+++ src/app/services/congress-api.service.ts
@@ -1,23 +1,38 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { map, tap } from "rxjs/operators";
+import { SpinnerService } from "../shared/services/spinner.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class CongressApiService {
-  baseUrl = "https://api.propublica.org/congress/v1/";
+  baseUrl = "https://api.propublica.org/congress/v1";
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private spinnerService: SpinnerService
+  ) {}
 
-  getAllMembers(congress: number, chamber: string): Observable<any> {
-    const url = `${this.baseUrl}${congress}/${chamber}/members.json`;
+  getAllMembers(): Observable<any> {
+    this.spinnerService.display(true);
+    const url = `${this.baseUrl}/115/senate/members.json`;
+    return this.http
+		.get(url)
+		.pipe(tap(() => this.spinnerService.display(false)));
+  }
+	
+  getSpecificMember(id): Observable<any> {
+		this.spinnerService.display(true);
+		const url = `${this.baseUrl}/members/${id}.json`;
     return this.http
       .get(url)
-      .pipe((response) => this.transformMembers(response));
+      .pipe(tap(()=> this.spinnerService.display(false)),
+				map((response) => this.transformSpecificMember(response)));
   }
 
-  transformMembers(response): Observable<any> {
-		return response.results;
-	}
+  transformSpecificMember(response) {
+    return response.results[0];
+  }
 }
